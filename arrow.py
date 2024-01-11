f782416a759f7f0dd2c328d83b754fe88afb8dc4
Kamil <kamil.dominko@gmail.com>
2024-01-11 18:55:31 +0100

    Dostosowanie strzał do sposobu przesuwania świata gry.

--- arrow.py
+++ arrow.py
@@ -13,10 +13,11 @@ class Arrow(pg.sprite.Sprite):
 
     def __init__(self, angle, x, y, offset):
         super().__init__()
+        self.offset = offset
         self.image = pg.transform.rotate(Arrow.image, angle - 90)
         self.rect = self.image.get_rect(center=(x, y))
         self.x, self.y = x, y
-        self.dx, self.dy = self._set_dx_dy(offset)
+        self.dx, self.dy = self._set_dx_dy(self.offset)
 
     def _set_dx_dy(self, offset):
         mosX, mosY = pg.mouse.get_pos() + offset
@@ -33,8 +34,19 @@ class Arrow(pg.sprite.Sprite):
     def _collide_screen_borders(self):
         """Sprawdza, czy strzała wyszła poza okno gry, jeżeli tak,
         to ją kasuje"""
-        if (self.rect.right < 0 or self.rect.left > SCREEN_WIDTH or
-                self.rect.bottom < 0 or self.rect.top > SCREEN_HEIGHT):
+        # _rect = self.rect.copy()
+        # _rect.topleft -= self.offset
+        # if (_rect.right < 0 or _rect.left > SCREEN_WIDTH or
+        #         _rect.bottom < 0 or _rect.top > SCREEN_HEIGHT):
+        #     self.kill()
+
+        # if (self.rect.right < 0 or self.rect.left > SCREEN_WIDTH or
+        #         self.rect.bottom < 0 or self.rect.top > SCREEN_HEIGHT):
+        #     self.kill()
+        if ((self.rect.right - self.offset.x < 0) or
+                (self.rect.left - self.offset.x > SCREEN_WIDTH) or
+                (self.rect.bottom - self.offset.y < 0) or
+                (self.rect.top - self.offset.y > SCREEN_HEIGHT)):
             self.kill()
 
     def _collide_enemies(self, enemies, text):
